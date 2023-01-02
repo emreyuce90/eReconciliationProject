1eeaa5215e0903c1248089b386950f52ec5437cf
Emre YÜCE <emreyuce90@hotmail.com>
2023-01-02 13:41:34 +0300

    *appsettings.json jwt ayarları oluşturuldu,cors eklendi,jwt bearer ayaralrı program cs e eklendi

--- Web.Api/Program.cs
+++ Web.Api/Program.cs
@@ -1,10 +1,25 @@
 using Business.IoC;
+using Core.Utilities.JWT;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddBLLDependencies();
 builder.Services.AddControllers();
+//Adding CORS
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("AllowOrigin",builder=>builder.WithOrigins("https://localhost:4200"));
+});
+//JWT registration
+var token = builder.Configuration.GetSection("TokenOptions").Get<TokenOptions>();
+builder.Services.AddAuthentication();
+
+
+
+
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,7 +32,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors(builder=>builder.WithOrigins("https://localhost:4200").AllowAnyHeader());
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
